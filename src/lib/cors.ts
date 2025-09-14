549899b75b53dcebb1c60265e74d92ba07a14b30
Aman <amankash777@gmail.com>
2025-09-14 20:51:25 +0530

    resolved any type

--- src/lib/cors.ts
+++ src/lib/cors.ts
@@ -1,4 +1,8 @@
-export async function withCors(req: Request, resBody: any, status = 200) {
+export async function withCors(
+    req: Request,
+    resBody: Record<string, unknown> | string | null,
+    status = 200
+) {
     if (req.method === "OPTIONS") {
         return new Response(null, {
             status: 204,
@@ -11,13 +15,17 @@ export async function withCors(req: Request, resBody: any, status = 200) {
         });
     }
 
-    return new Response(JSON.stringify(resBody), {
-        status,
-        headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        },
-    });
+    return new Response(
+        typeof resBody === "string" ? resBody : JSON.stringify(resBody),
+        {
+            status,
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods":
+                    "GET, POST, PUT, DELETE, OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            },
+        }
+    );
 }
